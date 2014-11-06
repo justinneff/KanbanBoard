1a1e9d3ed5757c115b3b90d9a42b2ce0d51d61fc
Justin Neff <neffjustin@gmail.com>
2014-11-05 23:03:12 -0600

    Removed jade html templating engine. Decided to go with pure html instead.

--- server/config/express.config.js
+++ server/config/express.config.js
@@ -7,8 +7,6 @@ module.exports = function(app, config) {
 	function compile(str, path) {
 		return stylus(str).set('filename', path);
 	}
-	app.set('views', config.rootPath + '/server/views');
-	app.set('view engine', 'jade');
 	app.use(logger('dev'));
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(bodyParser.json());
